3f87031df9bc1e38580b998fd22d12d727d81b3f
hyunjin_byun <shinbm219@naver.com>
2024-03-05 07:56:25 +0900

    사칙연산,10869,브론즈

---
+++ src/baekjoon/bronze/Bronze10869.java
@@ -0,0 +1,18 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Bronze10869 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
